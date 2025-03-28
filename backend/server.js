c49264f162391835ebe5e37f6a6c2e5f206c18e6
Yogesh Baisla <yogeshbaisla793@gmail.com>
2025-03-28 20:24:14 +0530

    adding port dynamic for render

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,7 @@ app.use('/api/sessions', sessionRoutes);
 mongoose.connect('mongodb://localhost:27017/myTypingApp', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('MongoDB connected');
-    app.listen(5000, () => console.log('Server running on port 5000'));
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch(err => console.error(err));
